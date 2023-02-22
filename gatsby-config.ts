cdaaf7bef7bca572bec3fd7c7697452f060dab0c
franguerrero <franguerrero@hotmail.com>
2023-02-22 12:34:48 +0100

    cambios para probar push

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Fran Guerrero Personal Site`,
+    siteUrl: `https://portal.frangue.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
